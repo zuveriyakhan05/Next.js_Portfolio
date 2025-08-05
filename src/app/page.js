1fce42861d3e2be0b2f2d6d531b47b519c017505
Zuveriya Khan <zuveriyap05@gmail.com>
2025-08-05 23:52:02 +0530

    Hero and Sidebar Completed

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,8 @@
 export default function HomePage() {
   return (
-    <section>
-      <h1 className="text-4xl font-bold mb-4">Hi, I'm Zuveriya Khan</h1>
-      <p className="mb-8 text-lg">Designer + Developer. Welcome to my portfolio!</p>
-    </section>
+    <div>
+      {/* <h1 className="text-4xl font-bold mb-4">Hi, I'm Zuveriya Khan</h1>
+      <p className="mb-8 text-lg">Designer + Developer. Welcome to my portfolio!</p> */}
+    </div>
   );
 }
