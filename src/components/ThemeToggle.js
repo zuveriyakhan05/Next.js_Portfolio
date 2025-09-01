ffd6c1bcc4072b0e45149e979c2b473c8587991a
Zuveriya Khan <zuveriyap05@gmail.com>
2025-09-01 21:55:06 +0530

    Next.js Portfolio

--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -2,11 +2,10 @@
 
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
-import { FaSun, FaMoon } from "react-icons/fa";
 
 export default function ThemeToggle() {
-  const { theme, setTheme, systemTheme } = useTheme();
-  const [mounted, setMounted] = useState(true);
+  const { setTheme, resolvedTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
     setMounted(true);
@@ -14,15 +13,13 @@ export default function ThemeToggle() {
 
   if (!mounted) return null;
 
-  const currentTheme = theme === "system" ? systemTheme : theme;
-
   return (
     <button
       aria-label="Toggle Dark Mode"
-      onClick={() => setTheme(currentTheme === "dark" ? "light" : "dark")}
+      onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
       className="fixed top-4 right-4 p-2 rounded-full bg-gray-200 dark:bg-gray-800 text-gray-800 dark:text-gray-200 shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition"
     >
-      {currentTheme === "dark" ? "☀️" :"🌙"}
+      {resolvedTheme === "dark" ? "☀️" : "🌙"}
     </button>
   );
 }
